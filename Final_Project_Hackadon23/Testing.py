b8cf446d10de1eb56c280810bdf74a83cca0aa10
Md Rafidul Islam Rafid <62617237+AvoidRafid@users.noreply.github.com>
2023-11-11 11:03:26 +0100

    Add files via upload

---
+++ Final_Project_Hackadon23/Testing.py
@@ -0,0 +1,159 @@
+import azure.cognitiveservices.speech as speechsdk
+import pyttsx3
+import weather_tip
+import routes
+import time
+import main
+import subprocess
+import webbrowser
+
+
+'''
+def main():
+    answer = "yes"
+    if answer == "yes":
+        subprocess.run(['python3', 'main.py']) #works
+        url = 'https://mail.google.com/mail/u/0/?tab=km#inbox'
+        webbrowser.open_new_tab(url)
+        
+    # Replace this with the URL you want to open
+    elif answer == "no":
+        pass
+# Replace 'file_to_run.py' with the name of the Python file you want to execute
+'''
+
+def speak_text(text, rate=130):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', rate)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def trigger(prompt):
+    speak_text(prompt)
+
+    subscription_key = "bb044baee5d7476e93585c526562fc27"
+    endpoint = "https://germanywestcentral.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, endpoint=endpoint)
+
+    audio_input = speechsdk.AudioConfig(use_default_microphone=True)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+    
+    while True:
+        result = speech_recognizer.recognize_once()
+
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            recognized_text = result.text.lower()
+            if any(word in recognized_text for word in ["genie", "jennie", "gini","jeanne", "giny", "jeannie", "ginie", "jinie", "ginee", "ginny", "geny", "giney", "ginnie"]):
+                speak_text("Jeenie is on the way!")
+                return "ginie"
+            else:
+                print(recognized_text)
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            continue
+
+
+'''
+def get_user_input(prompt):
+    speak_text(prompt)
+
+    # azure
+    subscription_key = "bb044baee5d7476e93585c526562fc27"
+    endpoint = "https://germanywestcentral.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, endpoint=endpoint)
+
+    # microphone input config
+    audio_input = speechsdk.AudioConfig(use_default_microphone=True)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+    result = speech_recognizer.recognize_once()
+
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        recognized_text = result.text
+        return recognized_text
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("Not recognised")
+'''
+
+def get_user_input(prompt, max_retries=2):
+    for retry in range(max_retries + 1):
+        speak_text(prompt)
+
+        # azure
+        subscription_key = "bb044baee5d7476e93585c526562fc27"
+        endpoint = "https://germanywestcentral.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+        speech_config = speechsdk.SpeechConfig(subscription=subscription_key, endpoint=endpoint)
+
+        # microphone input config
+        audio_input = speechsdk.AudioConfig(use_default_microphone=True)
+
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+        result = speech_recognizer.recognize_once()
+
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            recognized_text = result.text
+            return recognized_text
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            if retry < max_retries:
+                speak_text("Sorry, I didn't catch that. Could you say it again?")
+                time.sleep(1)
+            else:
+                print("Not recognised.")
+                break
+
+
+def main():
+    # waiting for trigger word "ginie"
+    trigger_word = None
+    while trigger_word != "ginie":
+        trigger_word = trigger("")
+    
+    # welcome message
+    speak_text("Welcome to the VGI. I am ready to process your request")
+
+    # getting user inputs
+    starting_point = get_user_input("What is your starting point?")    # Central
+    destination = get_user_input("What is your destination?")       # Square
+    hour = get_user_input("Input the desired hour")     # 14
+    minutes = get_user_input("Input the minutes")       #10
+
+    if starting_point:
+        print("Starting point:", starting_point)
+    if destination:
+        print("Destination:", destination)
+    if hour:
+        print("Hour", hour)
+    if minutes:
+        print("Minutes", minutes)
+
+    if hour and minutes:
+        time = hour.strip(".") + ":" + minutes.strip(".")
+    starting_point = starting_point.strip(".")
+    destination =  destination.strip(".")
+    # bus departure
+    route  = routes.calculate_time(starting_point, destination, time)
+    speak_text(route)
+    time.sleep(1)
+
+    # tip based on the current weather
+    for key, value in weather_tip.weather().items():
+        if key == "Advice":
+            tip = value
+            
+
+    speak_text("By the way, a quick tip for you:")
+    speak_text(tip)
+    answer = get_user_input("Would you like to see the live tracking of the bus?")
+    if answer.trim(".") == "yes":
+        subprocess.run(['python3', 'main.py']) #works
+        url = 'https://mail.google.com/mail/u/0/?tab=km#inbox'
+        webbrowser.open_new_tab(url)
+        
+    elif answer.trim(".") == "no":
+        speak_text("Okay! Genie wishes you a safe trip!")
+
+
+    
+if __name__ == "__main__":
+    main()
---
+++ Final_Project_Hackadon23/main.py
@@ -0,0 +1,100 @@
+import azure.cognitiveservices.speech as speechsdk
+import pyttsx3
+import weather_tip
+import routes
+import subprocess
+
+def speak_text(text, rate=130):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', rate)
+    engine.say(text)
+    engine.runAndWait()
+
+def trigger(prompt):
+    speak_text(prompt)
+
+    subscription_key = "bb044baee5d7476e93585c526562fc27"
+    endpoint = "https://germanywestcentral.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, endpoint=endpoint)
+
+    audio_input = speechsdk.AudioConfig(use_default_microphone=True)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+    
+    while True:
+        result = speech_recognizer.recognize_once()
+
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            recognized_text = result.text.lower()
+            if any(word in recognized_text for word in ["genie", "jennie", "gini", "giny", "jeannie", "ginie", "jinie", "ginee", "ginny", "geny", "giney"]):
+                speak_text("Ginie is on the way!")
+                return "ginie"
+            else:
+                print(recognized_text)
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            # Ignore unrecognized input and continue listening
+            continue
+
+def get_user_input(prompt):
+    speak_text(prompt)
+
+    # azure
+    subscription_key = "bb044baee5d7476e93585c526562fc27"
+    endpoint = "https://germanywestcentral.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, endpoint=endpoint)
+
+    # microphone input config
+    audio_input = speechsdk.AudioConfig(use_default_microphone=True)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+    result = speech_recognizer.recognize_once()
+
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        recognized_text = result.text
+        return recognized_text
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("Not recognised")
+
+def main():
+
+    trigger_word = None
+    while trigger_word != "ginie":
+        trigger_word = trigger("")
+    
+    speak_text("Welcome to the VGI. I am ready to process your request")
+
+    starting_point = get_user_input("What is your starting point?")
+    destination = get_user_input("What is your destination?")
+    hour = get_user_input("Input the desired hour")
+    minutes = get_user_input("Input the minutes")
+
+    if starting_point:
+        print("Starting point:", starting_point)
+    if destination:
+        print("Destination:", destination)
+    if hour:
+        print("Hour", hour)
+    if minutes:
+        print("Minutes", minutes)
+    
+    time = hour.strip(".") + ":" + minutes.strip(".")
+
+    route  = routes.calculate_time(starting_point, destination, time)
+    speak_text(route)
+
+    for key, value in weather_tip.weather().items():
+        if key == "Advice":
+            tip = value
+            print(tip)
+
+    speak_text("By the way, a quick tip for you:")
+    speak_text(tip)
+    answer = get_user_input("Would you like to see the live tracking of the bus?")
+    if answer == "yes":
+        subprocess.run(['python3', 'mmain.py'])
+    elif answer == "no":
+        speak_text("Okay! Genie wishes you a safe trip!")
+
+    speak_text("Ginie wishes you a happy trip!")
+
+
