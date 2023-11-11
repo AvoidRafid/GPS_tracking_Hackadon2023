ff05790bc34a95359a789c300f0ae708edd96a84
ksenijav03 <143193896+ksenijav03@users.noreply.github.com>
2023-11-11 10:02:41 +0100

    Update main.py
    Added second try for inputs.

--- Genie/main.py
+++ Genie/main.py
@@ -2,12 +2,15 @@ import azure.cognitiveservices.speech as speechsdk
 import pyttsx3
 import weather_tip
 import routes
+import time
+
 
 def speak_text(text, rate=130):
     engine = pyttsx3.init()
     engine.setProperty('rate', rate)
     engine.say(text)
     engine.runAndWait()
+
 
 def trigger(prompt):
     speak_text(prompt)
@@ -25,15 +28,16 @@ def trigger(prompt):
 
         if result.reason == speechsdk.ResultReason.RecognizedSpeech:
             recognized_text = result.text.lower()
-            if any(word in recognized_text for word in ["genie", "jennie", "gini", "giny", "jeannie", "ginie", "jinie", "ginee", "ginny", "geny", "giney"]):
-                speak_text("Ginie is on the way!")
+            if any(word in recognized_text for word in ["genie", "jennie", "gini","jeanne", "giny", "jeannie", "ginie", "jinie", "ginee", "ginny", "geny", "giney", "ginnie"]):
+                speak_text("Jeenie is on the way!")
                 return "ginie"
             else:
                 print(recognized_text)
         elif result.reason == speechsdk.ResultReason.NoMatch:
-            # Ignore unrecognized input and continue listening
             continue
 
+
+'''
 def get_user_input(prompt):
     speak_text(prompt)
 
@@ -53,19 +57,49 @@ def get_user_input(prompt):
         return recognized_text
     elif result.reason == speechsdk.ResultReason.NoMatch:
         print("Not recognised")
+'''
+
+def get_user_input(prompt, max_retries=2):
+    for retry in range(max_retries + 1):
+        speak_text(prompt)
+
+        # azure
+        subscription_key = "bb044baee5d7476e93585c526562fc27"
+        endpoint = "https://germanywestcentral.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+        speech_config = speechsdk.SpeechConfig(subscription=subscription_key, endpoint=endpoint)
+
+        # microphone input config
+        audio_input = speechsdk.AudioConfig(use_default_microphone=True)
+
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+        result = speech_recognizer.recognize_once()
+
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            recognized_text = result.text
+            return recognized_text
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            if retry < max_retries:
+                speak_text("Sorry, I didn't catch that. Could you say it again?")
+                time.sleep(1)
+            else:
+                print("Not recognised.")
+                break
+
 
 def main():
-
+    # waiting for trigger word "ginie"
     trigger_word = None
     while trigger_word != "ginie":
         trigger_word = trigger("")
     
+    # welcome message
     speak_text("Welcome to the VGI. I am ready to process your request")
 
-    starting_point = get_user_input("What is your starting point?")
-    destination = get_user_input("What is your destination?")
-    hour = get_user_input("Input the desired hour")
-    minutes = get_user_input("Input the minutes")
+    # getting user inputs
+    starting_point = get_user_input("What is your starting point?")    # Central
+    destination = get_user_input("What is your destination?")       # Square
+    hour = get_user_input("Input the desired hour")     # 14
+    minutes = get_user_input("Input the minutes")       #10
 
     if starting_point:
         print("Starting point:", starting_point)
@@ -75,26 +109,32 @@ def main():
         print("Hour", hour)
     if minutes:
         print("Minutes", minutes)
-    
-    time = hour.strip(".") + ":" + minutes.strip(".")
 
+    if hour and minutes:
+        time = hour.strip(".") + ":" + minutes.strip(".")
+    starting_point = starting_point.strip(".")
+    destination =  destination.strip(".")
+    # bus departure
     route  = routes.calculate_time(starting_point, destination, time)
     speak_text(route)
+    time.sleep(1)
 
+    # tip based on the current weather
     for key, value in weather_tip.weather().items():
         if key == "Advice":
             tip = value
-            print(tip)
+            
 
     speak_text("By the way, a quick tip for you:")
     speak_text(tip)
-    
     answer = get_user_input("Would you like to see the live tracking of the bus?")
-    if answer == "yes":
+    if answer.trim(".") == "yes":
         pass
-    elif answer == "no":
+    elif answer.trim(".") == "no":
         speak_text("Okay! Genie wishes you a safe trip!")
 
+
+    
 if __name__ == "__main__":
     main()
 
